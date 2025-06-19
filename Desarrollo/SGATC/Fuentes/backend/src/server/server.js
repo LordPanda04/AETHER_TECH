d7280986a633669429253cbfd991f7cf097f2a10
DavainCI <rodrigodavalos2105@gmail.com>
2025-06-19 15:04:50 -0500

    DAS

--- Desarrollo/SGATC/Fuentes/backend/src/server/server.js
+++ Desarrollo/SGATC/Fuentes/backend/src/server/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root', // Cambia por tu usuario de MySQL
-  password: '1234', // Cambia por tu contraseña
+  password: 'root', // Cambia por tu contraseña
   database: 'SistemaGestorAlmacen',
   port: 3306,
 });
